d48c3b373a2fab57abeedfb345288732b9ea24d4
hjcho <chohyo703@gmail.com>
2023-08-11 15:17:26 +0900

    Section06
    @ComponentScan, @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,10 @@
 package hello.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     /////MemberServiceImpl 은 MemberRepository, MemoryMemberRepository 둘다 의존하는 문제가 생김 - > 추상화에도 의존 구체화에도 의존
@@ -7,6 +12,8 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+
+    @Autowired // MemberRepository 타입에 맞는 스프링 빈을 주입  ac.getBean(MemberRepository.class)이랑 비슷
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
